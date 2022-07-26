cc2a1b63a26c7f86c2c41a277177f41fa41a6373
Borys <mb.787788@gmail.com>
2022-07-26 20:38:52 +0100

    graph2 model

--- code/graph_model.py
+++ code/graph_model.py
@@ -1,5 +1,7 @@
+import torch.nn.functional as F
 import os
 from pathlib import Path
+from urllib.parse import uses_relative
 import torch
 import torch.nn as nn
 import numpy as np
@@ -17,6 +19,7 @@ from state import State
 import torch
 
 from dataclasses import dataclass
+import itertools
 
 
 @dataclass
@@ -29,6 +32,18 @@ class Sample:
 class SampleWithLabel:
     sample: Sample
     label: float
+
+
+@dataclass
+class TwoSamples:
+    markdown: str
+    correct_code: str
+    wrong_code: str
+
+    def max_len(self):
+        l1 = len(self.markdown) + len(self.correct_code)
+        l2 = len(self.markdown) + len(self.wrong_code)
+        return max(l1, l2)
 
 
 max_tokenizer_len = 512
@@ -48,10 +63,11 @@ class MyGraphModel(nn.Module):
                 preload_state, map_location=state.device))
         self.name = preload_state if preload_state is not None else "0"
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, use_sigmoid=True):
         x = self.graph(input_ids=input_ids, attention_mask=attention_mask)[0]
         x = self.top(x[:, 0, :])
-        x = torch.sigmoid(x)
+        if use_sigmoid:
+            x = torch.sigmoid(x)
         return x
 
     def encode_sample(self, sample):
@@ -151,3 +167,56 @@ def train(state, model, dataset, save_to_wandb=False, optimizer_state=None):
         wandb.finish()
 
     model.save('cur-final', optimizer=optimizer)
+
+
+def train2(state, model, dataset, save_to_wandb=False, optimizer_state=None):
+    print('start training...')
+    np.random.seed(123)
+    learning_rate = 5e-5
+    optimizer = AdamW(model.parameters(), lr=learning_rate, eps=1e-8)
+    if optimizer_state is not None:
+        print('loading optimizer state...')
+        optimizer.load_state_dict(torch.load(optimizer_state))
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=0.05*len(dataset), num_training_steps=len(dataset))
+    model.train()
+    print('training... num batches:', len(dataset))
+    if save_to_wandb:
+        init_wandb(name='graph2-training')
+
+    for b_id, batch in enumerate(tqdm(dataset)):
+        samples = list(map(lambda x: [Sample(markdown=x.markdown, code=x.correct_code), Sample(
+            markdown=x.markdown, code=x.wrong_code)], batch))
+        samples = list(itertools.chain(*samples))
+        encoded = model.encode(state, samples)
+        input_ids = encoded['input_ids']
+        attention_mask = encoded['attention_mask']
+
+        optimizer.zero_grad()
+        pred = model(input_ids, attention_mask, use_sigmoid=False)
+
+        losses = []
+        for i in range(len(batch)):
+            score_correct = pred[i * 2 + 0]
+            score_wrong = pred[i * 2 + 1]
+            cur_scores = torch.tensor(
+                [score_wrong, score_correct], requires_grad=True)
+            losses.append(F.softmax(cur_scores, dim=0)[0])
+
+        total_loss = sum(losses) / len(batch)
+        total_loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        optimizer.step()
+        scheduler.step()
+        if save_to_wandb:
+            wandb.log({'graph_loss': total_loss.item()})
+
+        if (b_id % 1000 == 999):
+            print('Saving model after', b_id)
+            model.save('2-step-batch-' + str(b_id), optimizer=optimizer)
+
+    if save_to_wandb:
+        wandb.finish()
+
+    model.save('2-cur-final', optimizer=optimizer)
