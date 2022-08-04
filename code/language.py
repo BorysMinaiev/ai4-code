4af9364fb6d4d8b7101fb9ca30f654e62f939e45
Borys Minaiev <mb.787788@gmail.com>
2022-08-04 22:32:51 +0000

    get better langdetect strategy

--- code/language.py
+++ code/language.py
@@ -18,4 +18,13 @@ def detect_nb_lang(nb):
     langs = []
     for cell_id in markdowns:
         langs.append(detect_lang(nb.loc[cell_id]['source']))
-    return most_frequent(langs)
+    langs.sort()
+    best = most_frequent(langs)
+    if best == 'en':
+        without_en = list(filter(lambda a: a != 'en', langs))
+        if len(without_en) != 0:
+            best2 = most_frequent(without_en)
+            cnt = langs.count(best2)
+            if cnt > 2 and cnt * 3 >= len(langs):
+                return best2
+    return best
